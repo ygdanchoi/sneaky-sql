08ad51099ff21a19570d707f756493410abf59ef
ygdanchoi <ygdanchoi@gmail.com>
2017-05-02 10:56:10 -0400

    Add getter methods

--- lib/attr_accessor_object.rb
+++ lib/attr_accessor_object.rb
@@ -1,2 +1,11 @@
 class AttrAccessorObject
+  def self.my_attr_accessor(*ivars)
+    ivars.each do |ivar|
+      at_ivar = "@#{ivar}".to_sym
+
+      define_method(ivar) do
+        instance_variable_get(at_ivar)
+      end
+    end
+  end
 end
