0cdc999e0de21c3a0d23942fd4c27778ff03ddf6
Dan Choi <ygdanchoi@gmail.com>
2017-05-02 19:00:16 -0400

    Remove extraneous selfs

--- music.rb
+++ music.rb
@@ -3,41 +3,41 @@ require_relative './lib/sql_object'
 class Artist < SQLObject
   self.table_name = 'artists'
 
-  self.has_many :albums,
+  has_many :albums,
     class_name: 'Album',
     primary_key: :id,
     foreign_key: :artist_id
 
-  self.has_many_through :tracks, :albums, :tracks
+  has_many_through :tracks, :albums, :tracks
 
-  self.finalize!
+  finalize!
 end
 
 class Album < SQLObject
   self.table_name = 'albums'
 
-  self.belongs_to :artist,
+  belongs_to :artist,
     class_name: 'Artist',
     primary_key: :id,
     foreign_key: :artist_id
 
-  self.has_many :tracks,
+  has_many :tracks,
     class_name: 'Track',
     primary_key: :id,
     foreign_key: :album_id
 
-  self.finalize!
+  finalize!
 end
 
 class Track < SQLObject
   self.table_name = 'tracks'
 
-  self.belongs_to :album,
+  belongs_to :album,
     class_name: 'Album',
     primary_key: :id,
     foreign_key: :album_id
 
-  self.has_one_through :artist, :album, :artist
+  has_one_through :artist, :album, :artist
 
-  self.finalize!
+  finalize!
 end
--- music.sql
+++ music.sql
@@ -12,6 +12,7 @@ CREATE TABLE albums (
 
 CREATE TABLE tracks (
   id INTEGER PRIMARY KEY,
+  track INTEGER,
   title VARCHAR(255) NOT NULL,
   album_id INTEGER NOT NULL,
   FOREIGN KEY(album_id) REFERENCES album(id)
@@ -30,32 +31,32 @@ VALUES
   (2, "Graduation", 2);
 
 INSERT INTO
-  tracks (id, title, album_id)
+  tracks (id, track, title, album_id)
 VALUES
-  (1, "Mylo Xyloto", 1),
-  (2, "Hurts Like Heaven", 1),
-  (3, "Paradise", 1),
-  (4, "Charlie Brown", 1),
-  (5, "Us Against THe World", 1),
-  (6, "M.M.I.X.", 1),
-  (7, "Every Teardrop Is A Waterfall", 1),
-  (8, "Major Minus", 1),
-  (9, "U.F.O.", 1),
-  (10, "Princess Of China", 1),
-  (11, "Up In Flames", 1),
-  (12, "A Hopeful Transmission", 1),
-  (13, "Don't Let It Break Your Heart", 1),
-  (14, "Up With The Birds", 1),
-  (15, "Good Morning", 2),
-  (16, "Champion", 2),
-  (17, "Stronger", 2),
-  (18, "I Wonder", 2),
-  (19, "Good Life (Feat. T-Pain)", 2),
-  (20, "Can't Tell Me Nothing", 2),
-  (21, "Barry Bonds (Feat. Lil Wayne)", 2),
-  (22, "Drunk And Hot Girls (Feat. Mos Def)", 2),
-  (23, "Flashing Lights (Feat. Dwele)", 2),
-  (24, "Everything I Am (Feat. DJ Premier)", 2),
-  (25, "The Glory", 2),
-  (26, "Homecoming (Feat. Chris Martin)", 2),
-  (27, "Big Brother", 2);
+  (1, 1, "Mylo Xyloto", 1),
+  (2, 2, "Hurts Like Heaven", 1),
+  (3, 3, "Paradise", 1),
+  (4, 4, "Charlie Brown", 1),
+  (5, 5, "Us Against THe World", 1),
+  (6, 6, "M.M.I.X.", 1),
+  (7, 7, "Every Teardrop Is A Waterfall", 1),
+  (8, 8, "Major Minus", 1),
+  (9, 9, "U.F.O.", 1),
+  (10, 10, "Princess Of China", 1),
+  (11, 11, "Up In Flames", 1),
+  (12, 12, "A Hopeful Transmission", 1),
+  (13, 13, "Don't Let It Break Your Heart", 1),
+  (14, 14, "Up With The Birds", 1),
+  (15, 1, "Good Morning", 2),
+  (16, 2, "Champion", 2),
+  (17, 3, "Stronger", 2),
+  (18, 4, "I Wonder", 2),
+  (19, 5, "Good Life (Feat. T-Pain)", 2),
+  (20, 6, "Can't Tell Me Nothing", 2),
+  (21, 7, "Barry Bonds (Feat. Lil Wayne)", 2),
+  (22, 8, "Drunk And Hot Girls (Feat. Mos Def)", 2),
+  (23, 9, "Flashing Lights (Feat. Dwele)", 2),
+  (24, 10, "Everything I Am (Feat. DJ Premier)", 2),
+  (25, 11, "The Glory", 2),
+  (26, 12, "Homecoming (Feat. Chris Martin)", 2),
+  (27, 13, "Big Brother", 2);
