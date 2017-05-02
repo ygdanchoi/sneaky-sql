5b0c08bab8448f91d34c3180b0d9a2567212776c
ygdanchoi <ygdanchoi@gmail.com>
2017-05-02 15:40:51 -0400

    Let DBConnection work for first .sql file

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -1,10 +1,13 @@
 require 'sqlite3'
+require 'byebug'
 
-PRINT_QUERIES = ENV['PRINT_QUERIES'] == 'true'
 # https://tomafro.net/2010/01/tip-relative-paths-with-file-expand-path
 ROOT_FOLDER = File.join(File.dirname(__FILE__), '..')
-CATS_SQL_FILE = File.join(ROOT_FOLDER, 'cats.sql')
-CATS_DB_FILE = File.join(ROOT_FOLDER, 'cats.db')
+Dir.glob('*.sql') do |sql_file|
+  db_file = sql_file.split('.').first + '.db'
+  CATS_SQL_FILE ||= File.join(ROOT_FOLDER, sql_file)
+  CATS_DB_FILE ||= File.join(ROOT_FOLDER, db_file)
+end
 
 class DBConnection
   def self.open(db_file_name)
@@ -32,29 +35,14 @@ class DBConnection
   end
 
   def self.execute(*args)
-    print_query(*args)
     instance.execute(*args)
   end
 
   def self.execute2(*args)
-    print_query(*args)
     instance.execute2(*args)
   end
 
   def self.last_insert_row_id
     instance.last_insert_row_id
-  end
-
-  private
-
-  def self.print_query(query, *interpolation_args)
-    return unless PRINT_QUERIES
-
-    puts '--------------------'
-    puts query
-    unless interpolation_args.empty?
-      puts "interpolate: #{interpolation_args.inspect}"
-    end
-    puts '--------------------'
   end
 end
