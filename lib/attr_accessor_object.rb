574b858b1ee5a93b3cc3da34c986789b8addd095
ygdanchoi <ygdanchoi@gmail.com>
2017-05-02 10:57:01 -0400

    Add setter methods

--- lib/attr_accessor_object.rb
+++ lib/attr_accessor_object.rb
@@ -6,6 +6,10 @@ class AttrAccessorObject
       define_method(ivar) do
         instance_variable_get(at_ivar)
       end
+
+      define_method("#{ivar}=") do |val|
+        instance_variable_set(at_ivar, val)
+      end
     end
   end
 end
